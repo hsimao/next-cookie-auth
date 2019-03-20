022aef6704e179bcf263335b000ce36bfb2ec947
hsimao <e087754958@gmail.com>
2019-03-20 12:46:55 +0800

    add: default email, password

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -2,8 +2,8 @@ import { loginUser } from '../lib/auth'
 
 class LoginForm extends React.Component {
   state = {
-    email: '',
-    password: '',
+    email: 'Sincere@april.biz',
+    password: 'hildegard.org',
   }
 
   handleChange = event => {
@@ -20,13 +20,15 @@ class LoginForm extends React.Component {
   }
 
   render() {
+    const { email, password } = this.state
+
     return (
       <form onSubmit={this.handleSubmit}>
         <div>
-          <input type="email" name="email" placeholder="email" onChange={this.handleChange} />
+          <input type="email" name="email" value={email} placeholder="email" onChange={this.handleChange} />
         </div>
         <div>
-          <input type="password" name="password" placeholder="password" onChange={this.handleChange} />
+          <input type="password" name="password" value={password} placeholder="password" onChange={this.handleChange} />
         </div>
         <button type="submit">送出</button>
       </form>
